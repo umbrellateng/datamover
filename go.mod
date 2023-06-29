a9a279f26d132a01a5b834f9ab89361b6d23db11
liteng <liteng.zb@ccbft.com>
2023-06-29 15:37:08 +0800

    add dump or restore databases at multi-threaded mode

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module core.bank/datamover
 
 go 1.17
+
+require (
+	github.com/xelabs/go-mydumper v0.0.0-20210314012322-e6a0b54b5792
+	github.com/xelabs/go-mysqlstack v1.0.0
+)
+
+require (
+	github.com/shopspring/decimal v1.2.0 // indirect
+	gopkg.in/ini.v1 v1.62.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,25 +2,33 @@ package main
 
 import (
 	"flag"
-	"log"
+	"fmt"
 	"os"
 	"os/exec"
+
+	"github.com/xelabs/go-mydumper/common"
+	"github.com/xelabs/go-mydumper/config"
+	"github.com/xelabs/go-mysqlstack/xlog"
 )
 
-func main() {
+var (
+	log *xlog.Log = xlog.NewStdLog(xlog.Level(xlog.INFO))
 
 	// 定义命令行参数
-	var user string
-	var password string
-	var host string
-	var port string
-	var database string
-	var output string
-	var sqlFile string
+	user string
+	password string
+	host string
+	port string
+	database string
+	output string
+	sqlFile string
 
-	var all bool
-	var restore bool
+	all bool
+	restore bool
+	thread bool
+)
 
+func initFlags() {
 	flag.StringVar(&user, "user", "root", "mysql user")
 	flag.StringVar(&password, "password", "", "mysql password")
 	flag.StringVar(&host, "host", "127.0.0.1", "mysql host")
@@ -31,6 +39,7 @@ func main() {
 
 	flag.BoolVar(&all, "all-databases", false, "mysql all databases")
 	flag.BoolVar(&restore, "restore", false, "restore database from a sql file")
+	flag.BoolVar(&thread, "thread", false, "use multi-threaded mode")
 
 	// 定义短名称的参数，使用同一个变量地址
 	flag.StringVar(&user, "u", "root", "mysql user (shorthand)")
@@ -43,61 +52,133 @@ func main() {
 
 	flag.BoolVar(&all, "a", false, "mysql all databases")
 	flag.BoolVar(&restore, "r", false, "restore database from a sql file")
+	flag.BoolVar(&thread, "t", false, "use multi-threaded mode")
 
 	flag.Parse()
 
-
 	// 检查是否所有的 flag 都已经解析
 	if !flag.Parsed() {
-		log.Println("error: invalid flag format. Please use --flag or -f.")
+		log.Error("error: invalid flag format. Please use --flag or -f.")
 		os.Exit(3)
 	}
+}
+
+func main() {
+
+	initFlags()
+
 	var cmd *exec.Cmd
 	var err error
 
-	if restore {
-		if len(sqlFile) == 0 {
-			log.Fatal("Please provide the certain sql file")
-		}
+	if thread {
+		if restore {
+			restoreArgs := &config.Config{
+				User:            user,
+				Password:        password,
+				Address:         fmt.Sprintf("%s:%s", host, port),
+				Outdir:          output,
+				Threads:         16,
+				IntervalMs:      10 * 1000,
+				OverwriteTables: false,
+			}
+			log.Info("restore databases from the directory: " + output + " ...")
+			fmt.Println()
+			common.Loader(log, restoreArgs)
 
-		cmd = exec.Command("mysql", "-u", user, "-p"+password, "--host", host, "--port", port)
-		// 打开源文件，用于读取SQL语句
-		file, err := os.Open(sqlFile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-
-		// 将命令的标准输入重定向到文件对象
-		cmd.Stdin = file
-
-	} else {
-		if all {
-			log.Println("enter in all")
-			cmd = exec.Command("mysqldump", "-u", user, "-p"+password, "--host", host, "--port", port, "-A")
 		} else {
-			log.Println("enter in a certain database")
-			// 检查数据库名是否为空
-			if  len(database) == 0{
-				log.Fatal("Please provide at least one database name.")
+			// TODO: do not dependent config file
+			filePath := "./datamover.ini"
+			dumperArgs, err := config.ParseDumperConfig(filePath)
+			if err != nil {
+				log.Error("read the datamover ini config error!")
+				return
 			}
 
-			cmd = exec.Command("mysqldump", "-u", user, "-p"+password, "--host", host, "--port", port,"--databases", database)
+			if dumperArgs == nil {
+				log.Error("read the datamover ini config nil!")
+				return
+			}
+
+			dumperArgs.User = user
+			dumperArgs.Password = password
+			dumperArgs.Address = fmt.Sprintf("%s:%s", host, port)
+			if all {
+				log.Info("dump all databases into file " + output + " at multi-threaded mode...")
+				fmt.Println()
+			} else {
+				dumperArgs.DatabaseRegexp = ""
+				// TODO: multi databases
+				if len(database) == 0 {
+					log.Error("Please provide at least one database name.")
+					return
+				}
+				dumperArgs.Database = database
+				log.Info("dump database " + database + " into " + output +  " directory... " )
+				fmt.Println()
+			}
+
+			dumperArgs.Outdir = output
+			if _, err := os.Stat(dumperArgs.Outdir); os.IsNotExist(err) {
+				x := os.MkdirAll(dumperArgs.Outdir, 0o777)
+				common.AssertNil(x)
+			}
+			common.Dumper(log, dumperArgs)
 		}
 
-		f, err := os.Create(output)
+	} else {
+		if restore {
+			if len(sqlFile) == 0 {
+				log.Error("Please provide the certain sql file")
+				return
+			}
+
+			log.Info("restore the database from the certain sql file: " + sqlFile + " ...")
+			fmt.Println()
+
+			cmd = exec.Command("mysql", "-u", user, "-p"+password, "--host", host, "--port", port)
+			// 打开源文件，用于读取SQL语句
+			file, err := os.Open(sqlFile)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+			defer file.Close()
+
+			// 将命令的标准输入重定向到文件对象
+			cmd.Stdin = file
+
+		} else {
+			if all {
+				log.Info("dump all databases into file " + output + " ...")
+				fmt.Println()
+				cmd = exec.Command("mysqldump", "-u", user, "-p"+password, "--host", host, "--port", port, "-A")
+			} else {
+				// 检查数据库名是否为空
+				if  len(database) == 0{
+					log.Error("Please provide at least one database name.")
+					return
+				}
+
+				log.Info("dump the certain database " + database +  " into file " + output + " ...")
+				fmt.Println()
+				cmd = exec.Command("mysqldump", "-u", user, "-p"+password, "--host", host, "--port", port,"--databases", database)
+			}
+
+			f, err := os.Create(output)
+			if err != nil {
+				log.Error("create file error: " + err.Error())
+				return
+			}
+			defer f.Close()
+			cmd.Stdout = f
+		}
+
+		err = cmd.Run()
 		if err != nil {
-			log.Fatal("create file error: " + err.Error())
+			log.Info(cmd.String())
+			log.Error("cmd Run error: " + err.Error())
+			return
 		}
-		defer f.Close()
-		cmd.Stdout = f
 	}
 
-	err = cmd.Run()
-	if err != nil {
-		log.Println(cmd.String())
-		log.Fatal("cmd Run error: " + err.Error())
-	}
-
-	log.Println("Complete!")
+	log.Info("Success!")
 }
